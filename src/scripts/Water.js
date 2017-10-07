8c81a4d946fe77b8c009b77eddcdeaf0dd26fa6f
Torsten Compart <torsten.compart@scout24.com>
2017-10-07 18:00:50 +0200

    Adding text for Water.js

--- src/scripts/Water.js
+++ src/scripts/Water.js
@@ -1,3 +1,16 @@
 import React from "react";
 
-export default () => <div>Hallo</div>;
+export default () => <div>
+    <p>
+        Wartungsarbeiten und Neuanschlüsse im örtlichen Leitungsnetz verursachen regelmäßig Verschmutzungen des
+        Trinkwassers z.B. mit Erd- und Rostpartikeln. Diese gelangen - ohne geeigneten Schutz - in ihre Hausinstallation
+        und verursachen dort Ablagerungen, die zu Hygienemängeln und Verstopfungen führen können. Wir empfehlen daher
+        die Installation von Wasserfiltern.
+    </p>
+    <p>
+        Darüber hinaus gibt es inzwischen Wasserenthärtungsanlagen, die Ihre Installation auch vor dem Verschleiß durch
+        Kalkablagerungen schützen. Gerade bei den Härtegraden unserer Region ist es sinnvoll solche Anlagen einzusetzen.
+        Kalkablagerungen bieten einen Nährboden für Bakterien, beschädigen Ihre Armaturen und erzeugen unschöne Flecken.
+        Sprechen Sie uns einfach an, wir beraten Sie gern.
+    </p>
+</div>;
