14343a76dff3e4c48873f5b881123e902ea53064
Torsten Compart <torsten.compart@gmail.com>
2019-07-09 00:27:34 +0200

    Upgrading to react-scripts 2.0.3

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import './stylesheets/main.css'
 import App  from './scripts/App';
-
+import 'react-app-polyfill/ie11';
 
 ReactDOM.render(
     <App />,
